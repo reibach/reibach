633457e1424467c2d72923712c2c0260df0f43d5
gm <reibach@federa.de>
2016-03-14 15:42:56 +0100

    aktuelle Datenbank

--- sql/reibach.sql
+++ sql/reibach.sql
@@ -1,78 +1,88 @@
--- phpMyAdmin SQL Dump
--- version 4.2.12deb2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (x86_64)
 --
--- Host: localhost
--- Erstellungszeit: 30. Dez 2015 um 13:58
--- Server Version: 5.5.44-0+deb8u1
--- PHP-Version: 5.6.13-0+deb8u1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: reibach
+-- ------------------------------------------------------
+-- Server version	5.5.44-0+deb8u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Datenbank: `reibach`
+-- Table structure for table `article`
 --
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `article`
---
-
-CREATE TABLE IF NOT EXISTS `article` (
-`id` int(10) NOT NULL,
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `unit` varchar(10) DEFAULT NULL,
   `comment` text,
-  `price` double DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `price` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `article`
+-- Dumping data for table `article`
 --
 
-INSERT INTO `article` (`id`, `name`, `unit`, `comment`, `price`) VALUES
-(2, 'Stundensatz ', '?', '', 25);
-
--- --------------------------------------------------------
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` VALUES (1,'Stundensatz','EURO','',42);
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `bill`
+-- Table structure for table `bill`
 --
 
-CREATE TABLE IF NOT EXISTS `bill` (
-`id` int(10) NOT NULL,
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `customer_id` int(4) NOT NULL,
   `mandator_id` int(4) NOT NULL,
   `description` text,
   `price` double DEFAULT NULL,
   `pay_id` int(1) DEFAULT NULL,
-  `billdate` date DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=83 DEFAULT CHARSET=latin1;
+  `billdate` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `fk_customer` (`customer_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `bill`
+-- Dumping data for table `bill`
 --
 
-INSERT INTO `bill` (`id`, `customer_id`, `mandator_id`, `description`, `price`, `pay_id`, `billdate`) VALUES
-(82, 13, 1, 'sasas', 0, NULL, '2015-12-30'),
-(81, 13, 1, '', 0, NULL, '2015-12-09');
-
--- --------------------------------------------------------
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+INSERT INTO `bill` VALUES (1,1,1,'',0,NULL,'2016-03-08'),(2,1,1,'',0,NULL,'2016-03-10');
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `customer`
+-- Table structure for table `customer`
 --
 
-CREATE TABLE IF NOT EXISTS `customer` (
-`id` int(10) NOT NULL,
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `company` varchar(150) NOT NULL,
   `title` varchar(50) NOT NULL,
   `firstname` varchar(150) NOT NULL,
@@ -85,24 +95,31 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `tel` varchar(27) DEFAULT NULL,
   `fax` varchar(27) DEFAULT NULL,
   `mobil` varchar(27) DEFAULT NULL,
-  `comment` text
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  `comment` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `customer`
+-- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`id`, `company`, `title`, `firstname`, `lastname`, `street`, `housenumber`, `zipcode`, `place`, `email`, `tel`, `fax`, `mobil`, `comment`) VALUES
-(13, 'test', 'testAnrede', 'Vorna', 'Nachn', 'Buxhorner Weg ', '15', '27729', 'Holste', 'guenter@federa.de', '0175 ', '123456', '0175 2717291', '');
-
--- --------------------------------------------------------
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'Michael Ellmers','Herr','Michael','Ellmers','Mittelmoorstrasse ','55','28879','Grasberg','michaelellmers@gmx.de','04208 895 220','','0163 692 5151','');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `mandator`
+-- Table structure for table `mandator`
 --
 
-CREATE TABLE IF NOT EXISTS `mandator` (
-`id` int(10) NOT NULL,
+DROP TABLE IF EXISTS `mandator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mandator` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `company` varchar(150) NOT NULL,
   `slogan` varchar(255) DEFAULT NULL,
   `title` varchar(50) NOT NULL,
@@ -127,24 +144,31 @@ CREATE TABLE IF NOT EXISTS `mandator` (
   `taxnumber` varchar(27) DEFAULT NULL,
   `comment` text,
   `country` varchar(50) NOT NULL,
-  `vat` varchar(1) DEFAULT NULL
+  `vat` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `mandator`
+-- Dumping data for table `mandator`
 --
 
-INSERT INTO `mandator` (`id`, `company`, `slogan`, `title`, `firstname`, `lastname`, `street`, `housenumber`, `zipcode`, `place`, `email`, `website`, `tel`, `fax`, `mobil`, `bankname`, `bankaccount`, `bankcodenumber`, `iban`, `bic`, `taxoffice`, `vatnumber`, `taxnumber`, `comment`, `country`, `vat`) VALUES
-(1, 'UM', '', 'Herr', 'Udo', 'Mittler', 'Diedrich-Speckmann-Straße', '12', '28865', 'Lilienthal', 'E-Mail: udo@federa.de', '', 'Tel: +49(0)4298 30690', '', 'Mob: +49(0)172 8887028', 'Bank: KSK OHZ', 'IBAN: DE89291523001401080666', 'BIC: BRLADE21OHZ', 'ASASAS', 'DE9899898989', 'Finanzamt ', 'Osterholz-Scharmbeck', 'Steuer-Nr.: 36/130/11311', 'ASASAS', '', '0');
-
--- --------------------------------------------------------
+LOCK TABLES `mandator` WRITE;
+/*!40000 ALTER TABLE `mandator` DISABLE KEYS */;
+INSERT INTO `mandator` VALUES (1,'Hausmeisterservice','','Herr','Udo','Mittler','Diedrich-Speckmann-Straße','12','28865','Lilienthal','E-Mail: udo@federa.de','','Tel: +49(0)4298 30690','','Mob: +49(0)172 8887028','Bank: KSK OHZ','IBAN: DE02 2915 2300 1401 0809 71','BIC: BRLADE21OHZ','ASASAS','1401080971fdfdf','Finanzamt ','Osterholz-Scharmbeck','Steuer-Nr.: 36/130/14426','ASASAS','','not');
+/*!40000 ALTER TABLE `mandator` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Tabellenstruktur für Tabelle `position`
+-- Table structure for table `position`
 --
 
-CREATE TABLE IF NOT EXISTS `position` (
-`id` int(10) NOT NULL,
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `position` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `bill_id` int(4) NOT NULL,
   `pos_num` varchar(2) DEFAULT NULL,
   `quantity` double DEFAULT NULL,
@@ -153,161 +177,30 @@ CREATE TABLE IF NOT EXISTS `position` (
   `comment` text,
   `price` double DEFAULT NULL,
   `tax` double DEFAULT NULL,
-  `amount` double DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=94 DEFAULT CHARSET=latin1;
+  `amount` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `fk_bill` (`bill_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Daten für Tabelle `position`
+-- Dumping data for table `position`
 --
 
-INSERT INTO `position` (`id`, `bill_id`, `pos_num`, `quantity`, `unit`, `name`, `comment`, `price`, `tax`, `amount`) VALUES
-(92, 81, '1', 4, 'H', 'Stunden ', '', 20.53, 13.11, 82.12),
-(93, 82, '1', 1221, 'sds', 'asass', '', 2323, 452867.87, 2836383);
+LOCK TABLES `position` WRITE;
+/*!40000 ALTER TABLE `position` DISABLE KEYS */;
+INSERT INTO `position` VALUES (1,1,'1',24,'Std','Handwerkliche Unterstützung an Wohngebäude und Grundstück','HHandwerkliche Unterstützung an Wohngebäude und Grundstück',30,136.8,720),(7,2,'1',1,'St.','Dachdurchführung für Edelstahl-Schornstein mit Bleirand für Ziegeldach ','',105,19.95,105);
+/*!40000 ALTER TABLE `position` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `project`
---
-
-CREATE TABLE IF NOT EXISTS `project` (
-`id` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text,
-  `price` double DEFAULT NULL,
-  `startdate` date DEFAULT NULL,
-  `enddate` date DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `task`
---
-
-CREATE TABLE IF NOT EXISTS `task` (
-`id` int(10) NOT NULL,
-  `project_id` int(4) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `comment` text,
-  `effort` double DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `version`
---
-
-CREATE TABLE IF NOT EXISTS `version` (
-`id` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `version` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `version`
---
-
-INSERT INTO `version` (`id`, `name`, `version`) VALUES
-(1, 'db', 38);
-
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `article`
---
-ALTER TABLE `article`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- Indizes für die Tabelle `bill`
---
-ALTER TABLE `bill`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD KEY `fk_customer` (`customer_id`);
-
---
--- Indizes für die Tabelle `customer`
---
-ALTER TABLE `customer`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- Indizes für die Tabelle `mandator`
---
-ALTER TABLE `mandator`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- Indizes für die Tabelle `position`
---
-ALTER TABLE `position`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD KEY `fk_bill` (`bill_id`);
-
---
--- Indizes für die Tabelle `project`
---
-ALTER TABLE `project`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- Indizes für die Tabelle `task`
---
-ALTER TABLE `task`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD KEY `fk_project` (`project_id`);
-
---
--- Indizes für die Tabelle `version`
---
-ALTER TABLE `version`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `article`
---
-ALTER TABLE `article`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT für Tabelle `bill`
---
-ALTER TABLE `bill`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=83;
---
--- AUTO_INCREMENT für Tabelle `customer`
---
-ALTER TABLE `customer`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT für Tabelle `mandator`
---
-ALTER TABLE `mandator`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT für Tabelle `position`
---
-ALTER TABLE `position`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=94;
---
--- AUTO_INCREMENT für Tabelle `project`
---
-ALTER TABLE `project`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT für Tabelle `task`
---
-ALTER TABLE `task`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT für Tabelle `version`
---
-ALTER TABLE `version`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-14 15:41:57
