bbab7cf06b2718c6d1e05829b3dfca4c918f8246
reibach <reibach@federa.de>
2020-02-27 15:26:54 +0100

    main-local instellens in git

---
+++ frontend/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'xnBeBwpK4zlZgy5cQSMG9lmfhdXvDu3p',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '172.22.119.*','::1'] // adjust this to your needs
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '172.22.119.*','::1'] // adjust this to your needs
+    ];
+}
+
+return $config;
